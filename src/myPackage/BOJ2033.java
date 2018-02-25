c32c1fa88a42be512146c1d6b9abe2f6719b4407
unknown <a301dks@naver.com>
2018-02-25 18:36:08 +0900

    add Realization

--- src/myPackage/BOJ2033.java
+++ src/myPackage/BOJ2033.java
@@ -3,16 +3,10 @@ package myPackage;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class BOJ2839 {
+public class BOJ2033 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int N = Integer.parseInt(br.readLine());
-		int five = N / 5;
-		int three = (N % 5) / 3;
-		if (N % 3 == 0 && ((N % 5) % 3) != 0)
-			System.out.println(N / 3);
-		else
-			System.out.println(((N % 5) % 3) != 0 ? -1 : five + three);
+		
 	}
 }
