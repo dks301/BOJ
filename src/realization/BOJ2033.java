2e8df58223a9a56cea597dad068202ef07fb2850
unknown <a301dks@naver.com>
2018-02-25 20:52:24 +0900

    Realization

--- src/realization/BOJ2033.java
+++ src/realization/BOJ2033.java
@@ -1,4 +1,4 @@
-package myPackage;
+package realization;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -7,6 +7,18 @@ public class BOJ2033 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		int temp = N;
+		int i = 10;
 		
+		while (N > i) {
+			temp = temp % i / (i / 10);
+			if (temp >= 5)
+				N = ((N / i) + 1) * i;
+			else
+				N = (N / i) * i;
+			i *= 10;
+			temp = N;
+		}
+		System.out.println(N);
 	}
 }
