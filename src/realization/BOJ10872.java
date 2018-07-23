0c64758474684dc490a5b6095b2c5e42e573bc35
HyungjuAn <a301dks@naver.com>
2018-07-23 10:56:42 +0900

    BOJ

---
+++ src/realization/BOJ10872.java
@@ -0,0 +1,16 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ10872 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int result = 1;
+		for (int i = N; i > 1; i--) {
+			result *= i;
+		}
+		System.out.println(result);
+	}
+}
