58daa0d6d7606b6e52f36a166d5ef9e3ce587288
HyungjuAn <a301dks@naver.com>
2018-07-25 13:20:42 +0900

    180725

--- src/realization/BOJ2563.java
+++ src/realization/BOJ2563.java
@@ -9,22 +9,18 @@ public class BOJ2563 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int numOfPaper = Integer.parseInt(br.readLine());
 		boolean[][] coloredArea = new boolean[100][100];
+		int result = 0;
 		for (int i = 0; i < numOfPaper; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			for (int j = x; j < x + 10; j++) {
 				for (int k = y; k < y + 10; k++) {
-					coloredArea[j][k] = true;
+					if (!coloredArea[j][k]) {
+						coloredArea[j][k] = true;
+						result++;
+					}
 				}
-			}
-		}
-		
-		int result = 0;
-		for (int i = 0; i < 100; i++) {
-			for (int j = 0; j < 100; j++) {
-				if (coloredArea[i][j])
-					result++;
 			}
 		}
 		System.out.println(result);
