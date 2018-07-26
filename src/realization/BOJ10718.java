bfc2086ac3007707dbc966409e58e5f79528a66a
HyungjuAn <a301dks@naver.com>
2018-07-26 13:18:01 +0900

    180726

---
+++ src/realization/BOJ10718.java
@@ -0,0 +1,11 @@
+package success;
+
+public class BOJ10718 {
+	private static final String NEW_LINE = "\n";
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append("강한친구 대한육군").append(NEW_LINE);
+		sb.append("강한친구 대한육군").append(NEW_LINE);
+		System.out.println(sb.toString());
+	}
+}
