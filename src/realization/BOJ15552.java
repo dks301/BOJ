c5315036ff954f20116c4c6a10b95dfa4768fde8
HyungjuAn <a301dks@naver.com>
2018-07-27 17:27:10 +0900

    180727

---
+++ src/realization/BOJ15552.java
@@ -0,0 +1,21 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15552 {
+	private static final String NEW_LINE = "\n";
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))
+			.append(NEW_LINE);
+		}
+		System.out.println(sb.toString());
+	}
+}
