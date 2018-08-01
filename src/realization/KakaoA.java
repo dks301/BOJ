6fbcf67f4f4581044301b20c3d0286039af59451
HyungjuAn <a301dks@naver.com>
2018-08-01 15:51:29 +0900

    180801

---
+++ src/realization/KakaoA.java
@@ -0,0 +1,19 @@
+package success;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class KakaoA {
+	private static final String NEW_LINE = "\n";
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++)
+			sb.append("Welcome to Kakao Code Festival!").append(NEW_LINE);
+		System.out.println(sb.toString());
+	}
+}
