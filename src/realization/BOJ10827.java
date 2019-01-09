74e4936c078b1666a328908f3dabc41e8ffa5c70
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-09 18:37:54 +0900

    Create BOJ10827.java

---
+++ src/realization/BOJ10827.java
@@ -0,0 +1,12 @@
+package realization;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class BOJ10827 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigDecimal a = sc.nextBigDecimal();
+		System.out.println(a.pow(sc.nextInt()).toPlainString());
+	}
+}
