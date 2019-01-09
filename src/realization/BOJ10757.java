134ebeab24529cbfbbf0faa893ef2025c1229958
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-09 18:30:09 +0900

    Create BOJ10757.java

---
+++ src/realization/BOJ10757.java
@@ -0,0 +1,10 @@
+package realization;
+
+import java.util.Scanner;
+
+public class BOJ10757 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextBigInteger().add(sc.nextBigInteger()));
+	}
+}
