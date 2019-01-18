8753ae33a0262b33dff05759a2355ae59bab38be
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-18 18:38:18 +0900

    Create BOJ10799.java

---
+++ src/myPackage/BOJ10799.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10799 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] input = br.readLine().toCharArray();
+		int size = input.length;
+		
+		Stack<boolean> s = new Stack<>();
+		for (int i = 0; i < size; i++) {
+			
+		}
+	}
+}
