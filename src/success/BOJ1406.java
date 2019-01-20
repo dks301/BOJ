db1eeaf08b90af11723a3742edf0afe41703b6b3
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-20 16:19:59 +0900

    Update BOJ1406.java

--- src/success/BOJ1406.java
+++ src/success/BOJ1406.java
@@ -10,45 +10,42 @@ public class BOJ1406 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char[] str = br.readLine().toCharArray(); // 영어 소문자, <= 100,000
 		int N = Integer.parseInt(br.readLine()); // 1 <= N <= 500,000
-		Stack<Character> s1 = new Stack<>();
-		Stack<Character> s2 = new Stack<>();
+		Stack<Character> left = new Stack<>();
+		Stack<Character> right = new Stack<>();
 		
 		for (int i = 0; i < str.length; i++) {
-			s1.push(str[i]);
+			left.push(str[i]);
 		}
 		for (int i = 0; i < N; i++) {
 			String cmd = br.readLine();
 			switch (cmd.charAt(0)) {
 			case 'L':
-				if (s1.isEmpty()) {}
-				else {
-					s2.push(s1.pop());
+				if (!left.isEmpty()) {
+					right.push(left.pop());
 				}
 				break;
 			case 'D':
-				if (s2.isEmpty()) {}
-				else {
-					s1.push(s2.pop());
+				if (!right.isEmpty()) {
+					left.push(right.pop());
 				}
 				break;
 			case 'B':
-				if (s1.isEmpty()) {}
-				else {
-					s1.pop();
+				if (!left.isEmpty()) {
+					left.pop();
 				}
 				break;
 			case 'P':
-				s1.push(cmd.charAt(2));
+				left.push(cmd.charAt(2));
 				break;
 			}
 		}
-		while (!s2.isEmpty()) {
-			s1.push(s2.pop());
+		while (!left.isEmpty()) {
+			right.push(left.pop());
 		}
-		int size = s1.size();
+
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < size; i++) {
-			sb.append(s1.get(i));
+		while (!right.isEmpty()) {
+			sb.append(right.pop());
 		}
 		System.out.println(sb.toString());
 	}
