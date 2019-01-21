52d64c859d36229d10d8126426909f657fecd35e
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-21 17:37:04 +0900

    Create BOJ9095.java

---
+++ src/myPackage/BOJ9095.java
@@ -0,0 +1,17 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9095 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int[] d = new int[11];
+		for (int i = 0; i < T; i++) {
+			int n = Integer.parseInt(br.readLine());
+			
+		}
+	}
+}
