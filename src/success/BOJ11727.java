7a3c63147d97ad3459f809ebbd4bbc3cb0d5b500
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-21 17:16:28 +0900

    Update BOJ11727.java
    final

--- src/success/BOJ11727.java
+++ src/success/BOJ11727.java
@@ -1,4 +1,4 @@
-package myPackage;
+package success;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,11 +8,14 @@ public class BOJ11727 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		
 		int[] d = new int[n + 1];
 		d[0] = 1;
-		d[1] = 2;
-		for (int i = 2; i <= n; i++) {
-			d[i] = (d[i - 2] + d[i - 2] + d[i - 1]) % 10007;
+		d[1] = 1;
+		if (n > 1) {
+			for (int i = 2; i <= n; i++) {
+				d[i] = (2 * d[i - 2] + d[i - 1]) % 10007;
+			}
 		}
 		System.out.println(d[n]);
 	}
