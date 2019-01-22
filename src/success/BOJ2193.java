03424dd37a185b5ba4c8655b4f9fa8c3444d42f6
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-22 16:53:14 +0900

    Update BOJ2193.java
    Speed: long than BigInteger

--- src/success/BOJ2193.java
+++ src/success/BOJ2193.java
@@ -1,4 +1,4 @@
-package myPackage;
+package success;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,5 +8,13 @@ public class BOJ2193 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+
+		long[] d = new long[91];
+		d[0] = 0;
+		d[1] = 1;
+		for (int i = 2; i <= N; i++) {
+			d[i] = d[i - 1] + d[i - 2];
+		}
+		System.out.println(d[N]);
 	}
 }
