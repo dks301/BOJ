30e3c18342fc9327f88525585bab3a60c9322936
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-22 17:13:42 +0900

    Create BOJ10844.java

---
+++ src/myPackage/BOJ10844.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10844 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+	}
+}
