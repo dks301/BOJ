979c8e532284a635e73a33ce8b28fe934656374e
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-22 15:54:19 +0900

    Update BOJ11052.java
    final

--- src/success/BOJ11052.java
+++ src/success/BOJ11052.java
@@ -1,4 +1,4 @@
-package myPackage;
+package success;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,19 +10,21 @@ public class BOJ11052 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[] p = new int[N + 1];
-		int[] d = new int[N + 1];
-		
 		p[0] = 0;
-		d[0] = 0;
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		p[1] = Integer.parseInt(st.nextToken());
-		d[1] = p[1];
-		for (int i = 2; i <= N; i++) {
+		for (int i = 1; i <= N; i++) {
 			p[i] = Integer.parseInt(st.nextToken());
-			d[i] = Math.max(d[i - 1] + d[1], p[i]);
-			System.out.print("P[" + i + "]: " + p[i]);
-			System.out.println(" D[" + i + "]: " + d[i]);
+		}
+		int[] d = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			int max = 0;
+			for (int j = 1; j <= i; j++) {
+				if (max < d[i - j] + p[j]) {
+					max = d[i - j] + p[j];
+				}
+			}
+			d[i] = max;
 		}
 		System.out.println(d[N]);
 	}
