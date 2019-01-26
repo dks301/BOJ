feda128ca2faf9ef1a5a9e399f69a5155755c73b
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-26 19:32:11 +0900

    Update BOJ11053.java

--- src/myPackage/BOJ11053.java
+++ src/myPackage/BOJ11053.java
@@ -15,5 +15,16 @@ public class BOJ11053 {
 		for (int i = 1; i <= N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		int[] d = new int[N + 1];
+		d[1] = 1;
+		int maxIdx = 1;
+		for (int i = 2; i <= N; i++) {
+			if (A[i] > A[maxIdx]) {
+				d[i] = d[maxIdx] + 1;
+				maxIdx = i;
+			}
+		}
+		System.out.println(d[maxIdx]);
 	}
 }
