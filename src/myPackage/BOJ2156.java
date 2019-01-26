78d2b29605911c00292a7b149bbf90269cb25b9c
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-26 17:29:53 +0900

    Create BOJ2156.java

---
+++ src/myPackage/BOJ2156.java
@@ -0,0 +1,28 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2156 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] podo = new int[n + 1];
+		
+		for (int i = 1; i <= n; i++) {
+			podo[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int[][] d = new int[n + 1][2];
+		d[0][0] = 0;
+		d[0][1] = 0;
+		d[1][0] = 0;
+		d[1][1] = podo[1];
+		for (int i = 2; i <= n; i++) {
+			d[i][0] = d[i - 1][1] + d[i - 2][1];
+			d[i][1] = Math.max(d[i - 1][1] + podo[i], d[i - 2][1] + podo[i]);
+			System.out.println("d[" + i + "][0]: " + d[i][0] + " d[" + i + "][1]: " + d[i][1]);
+		}
+	}
+}
