82afc0d06ddfef682abf73389fb7439c962c73d2
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-29 18:43:10 +0900

    Update BOJ1699.java

--- src/dp/BOJ1699.java
+++ src/dp/BOJ1699.java
@@ -1,4 +1,4 @@
-package myPackage;
+package dp;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,8 +10,14 @@ public class BOJ1699 {
 		int N = Integer.parseInt(br.readLine());
 		
 		int[] d = new int[N + 1];
-		
-		
+		for (int i = 1; i <= N; i++) {
+			d[i] = i;
+			for (int j = 1; j * j <= i; j++) {
+				if (d[i] > d[i - j * j] + 1) {
+					d[i] = d[i - j * j] + 1;
+				}
+			}
+		}
 		System.out.println(d[N]);
 	}
 }
