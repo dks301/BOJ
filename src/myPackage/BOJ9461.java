978975527feb49cea006273f9f92ce32ae6f7ded
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-01-30 17:05:14 +0900

    Create BOJ9461.java

---
+++ src/myPackage/BOJ9461.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ9461 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+		}
+	}
+}
