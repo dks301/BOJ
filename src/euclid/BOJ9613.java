aeac20c958d21c63fe2f4422e8b5290b754f9802
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-01 14:23:18 +0900

    Update BOJ9613.java

--- src/euclid/BOJ9613.java
+++ src/euclid/BOJ9613.java
@@ -1,10 +1,12 @@
-package myPackage;
+package euclid;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ9613 {
+	private static final String NEW_LINE = "\n";
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
@@ -12,9 +14,21 @@ public class BOJ9613 {
 		StringBuilder sb = new StringBuilder();
 		while (t-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			System.out.println(st.countTokens());
-			System.out.println(st.nextToken());
+			long total = 0;
+			int size = Integer.parseInt(st.nextToken());
+			int[] input = new int[size];
+			for (int i = 0; i < size; i++) {
+				input[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			for (int i = 0; i < size - 1; i++) {
+				for (int j = i + 1; j < size; j++) {
+					total += gcd(input[i], input[j]);
+				}
+			}
+			sb.append(total).append(NEW_LINE);
 		}
+		System.out.println(sb.toString());
 	}
 	
 	public static int gcd(int a, int b) {
