030b9911ccc7b00512e40de42ac6b12298faee39
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-01 13:41:19 +0900

    BOJ2609

--- src/euclid/BOJ2609.java
+++ src/euclid/BOJ2609.java
@@ -1,4 +1,4 @@
-package myPackage;
+package euclid;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -10,5 +10,22 @@ public class BOJ2609 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
+		int gcd = gcd(a, b);
+		
+		System.out.println(gcd + "\n" + lcm(a, b, gcd));
+	}
+	
+	public static int gcd(int a, int b) {
+		int gcd = 0;
+		while (b != 0) {
+			gcd = b;
+			b = a % b;
+			a = gcd;
+		}
+		return gcd;
+	}
+	
+	public static int lcm(int a, int b, int gcd) {
+		return a * b / gcd;
 	}
 }
