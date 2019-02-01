3c2c0096bd7b977cc7ffa98746469bf28d8f2094
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-01 15:21:56 +0900

    Create BOJ2745.java

---
+++ src/myPackage/BOJ2745.java
@@ -0,0 +1,27 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2745 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		char[] N = st.nextToken().toCharArray();
+		int B = Integer.parseInt(br.readLine());
+		
+		int result = 0;
+		int len = N.length;
+		for (int i = 0; i < len; i++) {
+			int temp = (int)N[i];
+			if (temp >= 65) {
+				temp -= 55;
+			} else {
+				temp -= 48;
+			}
+			result += temp * (int)Math.pow(B, len - i - 1);
+		}
+		System.out.println(result);
+	}
+}
