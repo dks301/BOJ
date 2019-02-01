28784918470deaef5315528907684cf49e00fd4f
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-01 15:34:33 +0900

    Update BOJ11005.java

--- src/math/BOJ11005.java
+++ src/math/BOJ11005.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
-
+/*
+ * 진법 변환2
+ * 10진법 수 N을, B진법 수로  바꿔 출력
+ * A:10, B:11, ..., F:15, ..., Z:35
+ */
 public class BOJ11005 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
