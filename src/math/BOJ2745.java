348de47966f999d454ee2d81dfbd5b179c63240d
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-01 15:33:04 +0900

    Update BOJ2745.java

--- src/math/BOJ2745.java
+++ src/math/BOJ2745.java
@@ -3,7 +3,11 @@ package math;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/*
+ * 진법 변환
+ * B진법 수 N이 주어질 때, 이 수를 10진법으로 바꿔 출력
+ * A:10, B:11, ..., F:15, ..., Z:35
+ */
 public class BOJ2745 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
