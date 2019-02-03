417ea20f0babe60953b25ea12839a4bef2ceb489
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-03 19:11:31 +0900

    Update BOJ1676.java

--- src/math/BOJ1676.java
+++ src/math/BOJ1676.java
@@ -8,25 +8,19 @@ import java.io.InputStreamReader;
  * N!의 뒤에서부터 처음 0이 아닌 숫자가 나올 때까지 0의 갯수 출력
  */
 public class BOJ1676 {
-	private static final int TWO = 2;
 	private static final int FIVE = 5;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		int cntTwo = 0;
 		int cntFive = 0;
 		for (int i = 1; i <= N; i++) {
 			int temp = i;
-			while (temp % TWO == 0) {
-				temp /= TWO;
-				cntTwo++;
-			}
 			while (temp % FIVE == 0) {
 				temp /= FIVE;
 				cntFive++;
 			}
 		}
-		System.out.println(Math.min(cntTwo, cntFive));
+		System.out.println(cntFive);
 	}
 }
