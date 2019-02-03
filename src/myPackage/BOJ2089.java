8253950c063077ff7bd4b957ba7969b6a4d7393d
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-03 13:19:44 +0900

    Create BOJ2089.java

---
+++ src/myPackage/BOJ2089.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+ * -2진수
+ * (-2)^0=1, (-2)^1 =-2
+ * -2진수: 1, 110, 111, 100, 101 -> 10진수: 1 ,2 ,3, 4
+ */
+public class BOJ2089 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+	}
+}
