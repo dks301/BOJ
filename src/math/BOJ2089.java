1893952ac828eb43f8dd613c2593c08b06df7282
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-03 16:06:55 +0900

    Update BOJ2089.java

--- src/math/BOJ2089.java
+++ src/math/BOJ2089.java
@@ -1,4 +1,4 @@
-package myPackage;
+package math;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,5 +12,20 @@ public class BOJ2089 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		if (N == 0) {
+			System.out.println(0);
+			System.exit(0);
+		}
+		StringBuilder sb = new StringBuilder();
+		while (N != 0) {
+			int r = N % (-2);
+			N = N / (-2);
+			if (r == -1) {
+				r = 1;
+				N++;
+			}
+			sb.append(r);
+		}
+		System.out.println(sb.reverse().toString());
 	}
 }
