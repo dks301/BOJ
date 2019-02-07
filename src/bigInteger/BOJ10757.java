1bf00e5d5f86cc49ef5a7508b7911f13813e4058
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-07 16:15:43 +0900

    Update BOJ10757.java

--- src/bigInteger/BOJ10757.java
+++ src/bigInteger/BOJ10757.java
@@ -2,6 +2,10 @@ package bigInteger;
 
 import java.util.Scanner;
 
+/*
+ * 큰 수 A + B
+ */
+
 public class BOJ10757 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
