f0f5f26879c06f1e4e4f7d74158bd85f2743cefa
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-07 17:51:22 +0900

    Update BOJ10989.java

--- src/sort/BOJ10989.java
+++ src/sort/BOJ10989.java
@@ -3,7 +3,10 @@ package sort;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
+/*
+ * 수 정렬하기3
+ * N개의 수가 주어질 때, 오름차순으로 정렬 후 출력
+ */
 public class BOJ10989 {
 	private static final String NEW_LINE = "\n";
 	
