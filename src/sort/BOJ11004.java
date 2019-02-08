cfe4fdc92c38c27e8580fe18b3acdc9a6c31a316
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-08 18:08:12 +0900

    Create BOJ11004.java

---
+++ src/sort/BOJ11004.java
@@ -0,0 +1,27 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+/*
+ * K번째 수
+ * 수열 An이 주어질 때, 오름차순 정렬 후 k번째에 있는 수 출력 
+ */
+public class BOJ11004 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] A = new int[N + 1];
+		A[0] = Integer.MIN_VALUE;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(A);
+		System.out.println(A[K]);
+	}
+}
