3028ef37b3a9b7e89284979af232aa67ddc97e89
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-02-11 16:43:03 +0900

    Create BOJ2667.java

---
+++ src/myPackage/BOJ2667.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+import java.io.BufferedReader;
+/*
+ * 단지번호붙이기
+ */
+public class BOJ2667 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamreader(System.in));
+		
+	}
+}
