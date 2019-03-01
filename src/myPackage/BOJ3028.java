2c173744cd67628cb4ffb49b4ebce8d4b28891eb
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-03-01 16:32:43 +0900

    Create BOJ3028.java

---
+++ src/myPackage/BOJ3028.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+ * 창영마을
+ * A: 1과 2를 바꾸기
+ * B: 2와 3을 바꾸기
+ * C: 1과 3을 바꾸기
+ * 컵을 섞은 방법이 주어질 때, 어떤 컵에 공이 있는지 출력
+ */
+public class BOJ3028 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
