87be9198cfcae098d3e93e40c48722ec42b1a8f9
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-03-02 17:07:39 +0900

    Create BOJ14654.java

---
+++ src/myPackage/BOJ14654.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * 스테판 쿼리
+ * 총 N라운드(1<=N<=300), 가위는 1, 바위는 2, 보는 3
+ * 진 사람은 탈락, 이긴 사람이 계속 게임을 진행하는 서바이벌
+ * 단, 비긴 경우에는 새로 출전한 사람이 승리한 것으로 간주
+ * 첫판에는 비기는 사람이 없다
+ * 두 팀이 가위바위보 대회를 할 때, 최대 연승의 수 출력
+ */
+public class BOJ14654 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
