b353669be8529c2289b78831cf8eb1f9faf04d25
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-03-04 16:34:19 +0900

    Create BOJ9311.java

---
+++ src/myPackage/BOJ9311.java
@@ -0,0 +1,17 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * Robot in a Maze
+ * first Line: TC, Second Line: R and C(ROW & COL)
+ * X: wall, S: start, G: exit
+ * output: "Shortest Path: t" or "No Exit"
+ * What is the fewest number of moves the robot can make to exit from the maze?
+ */
+public class BOJ9311 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
