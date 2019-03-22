d20e6adab4b369f1230be480caca164f6171d1b7
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-03-22 14:09:27 +0900

    Create SEA2047.java

---
+++ src/realization/SEA2047.java
@@ -0,0 +1,12 @@
+package realization;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SEA2047 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String in = br.readLine();
+		System.out.print(in.toUpperCase());
+	}
+}
