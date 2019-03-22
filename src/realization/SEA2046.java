c9e31883f46d30a8c3cb00ddb923e5ddfba863a4
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-03-22 14:05:04 +0900

    Create SEA2046.java

---
+++ src/realization/SEA2046.java
@@ -0,0 +1,19 @@
+package realization;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SEA2046 {
+	private static final String STAMP = "#";
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		while (n-- > 0) {
+			sb.append(STAMP);
+		}
+		System.out.println(sb);
+	}
+}
