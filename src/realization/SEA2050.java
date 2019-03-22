194c0f0277bfd81da966c3ad54ff4e31a1c3b580
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-03-22 14:16:34 +0900

    Create SEA2050.java

---
+++ src/realization/SEA2050.java
@@ -0,0 +1,19 @@
+package realization;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SEA2050 {
+	private static final String SPACE = " ";
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] in = br.readLine().toCharArray();
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < in.length; i++) {
+			sb.append((int)(in[i] - 64)).append(SPACE);
+		}
+		System.out.print(sb);
+	}
+}
