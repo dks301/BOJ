43e5e24f5476c540fcc27aa8266bb78255027a4f
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-01 17:39:26 +0900

    Create BOJ2160.java

---
+++ src/myPackage/BOJ2160.java
@@ -0,0 +1,17 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+ * 그림 비교
+ * N(2<=N<=50)개의 그림이 있다.
+ * 각 그림은 5x7 크기이고, 두 가지 색으로 되어 있다. ('X' or '.')
+ * 가장 비슷한 두 개의 그림을 출력
+ */
+public class BOJ2160 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
