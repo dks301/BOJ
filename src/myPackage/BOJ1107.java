1d1b76ffe1029b7280a0bfea1a954b1a4b6b8a5a
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-03 16:53:38 +0900

    Create BOJ1107.java

---
+++ src/myPackage/BOJ1107.java
@@ -0,0 +1,14 @@
+package myPackage;
+/*
+ * 리모컨
+ * 버튼: 0~9, +, -
+ * 채널 0에서 -를 누른 경우 채널 변경안됨, 채널은 무한대만큼 존재
+ * 현재 채널은 100번
+ * N(0<=N<=500,000), 고장난 버튼의 수M(0<=M<=10)
+ * 채널 N으로 이동하기 위해 버튼을 최소 몇 번 눌러야하는지 출력 
+ */
+public class BOJ1107 {
+	public static void main(String[] args) throws Exception {
+		
+	}
+}
