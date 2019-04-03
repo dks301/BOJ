aacd7c850c908a81186e90df293b4e0e5a5733d7
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-03 14:56:04 +0900

    Create BOJ2902.java

---
+++ src/myPackage/BOJ2902.java
@@ -0,0 +1,14 @@
+package myPackage;
+/*
+ * KMP는 왜 KMP일까?
+ * Knuth-Morris-Pratt: 긴형태
+ * KMP: 짧은 형태
+ * 입력은 최대 100글자의 알파벳 대소문자, 하이픈'-'로만 이루어져있다.
+ * 첫글자와 하이픈 뒤는 항상 대문자, 그 외는 모두 소문자
+ * 긴 형태가 주어질 때, 짧은 형태를 출력
+ */
+public class BOJ2902 {
+	public static void main(String[] args) throws Exception {
+		
+	}
+}
