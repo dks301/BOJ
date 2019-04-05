e1e81ac51a49c2d0bfac26330c13913bdb6d39f7
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-05 14:44:46 +0900

    Create BOJ12100.java

---
+++ src/myPackage/BOJ12100.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * 2048(Easy)
+ * N*N 보드의 2048게임(1<=N<=20)
+ * 0은 빈칸, 이외의 값은 블록
+ * 블록은 2보다 크거나 같고, 1024보다 작거나 같은 2의 제곱꼴
+ */
+public class BOJ12100 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+	}
+}
