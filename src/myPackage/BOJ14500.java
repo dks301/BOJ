e16a01ecde9e5503439921e5570426a05ddd0432
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-06 17:09:53 +0900

    Create BOJ14500.java

---
+++ src/myPackage/BOJ14500.java
@@ -0,0 +1,20 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * 테트로미노
+ * 정사각형 4개를 이어붙인 것이 테트로미노(테트리스모양)
+ * 테트로미노는 회전, 대칭이 가능
+ * 크기가 N*M인 종이 위에 테트로미노 하나 놓기, 종이의 한칸에는 정수가 쓰여있음
+ * 테트로미노 하나가 놓인 칸의 최대값 출력
+ * 
+ * 첫째줄: 종이의 세로N, 가로M(4<=N,M<=500)
+ * 둘째줄부터 N개줄: 종이에 쓰여있는 수, 수는 1,000을 넘지않는 자연수
+ */
+public class BOJ14500 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
