a29acfc9dc6b5817a21d27a38c79fd301ef08874
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-07 14:39:54 +0900

    Create BOJ14502.java

---
+++ src/myPackage/BOJ14502.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * 연구소
+ * 연구소는 크기가 N * M(3<=N,M<=8)
+ * 바이러스는 상하좌우 인접한 빈 칸으로 퍼져나갈 수 있다.
+ * 새로 새울 수 있는 벽은 3개이며 3개를 모두 꼭 세워야함
+ * 0은 빈 칸(3개이상), 1은 벽, 2는 바이러스(2<=바이러스의 갯수<=10)
+ * 벽을 3개 세운 뒤, 바이러스가 퍼질수 없는 안전영역의 최댓값 출력
+ */
+public class BOJ14502 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
