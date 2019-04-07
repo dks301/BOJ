8b5bc289949d4c0765c645e15b585cbb5085fdc9
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-07 17:59:18 +0900

    Create BOJ14501.java

---
+++ src/myPackage/BOJ14501.java
@@ -0,0 +1,20 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+ * 퇴사
+ * N+1일째 되는 날 퇴사, N일 동안 최대한 많은 상담 하기
+ * 상담 기간 Ti, 금액 Pi
+ * 퇴사 전 얻을 수 있는 최대 수익 출력
+ * 
+ * 첫째줄: 1<=N<=15
+ * 둘째줄부터 N개줄: Ti(1<=Ti<=5) Pi(1<=Pi<=1,000), 1일부터 N일까지 순서대로
+ */
+public class BOJ14501 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
