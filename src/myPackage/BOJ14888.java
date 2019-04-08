18c8dfaedc37474092b51e4f556c44059a8931ae
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-08 13:35:00 +0900

    Create BOJ14888.java

---
+++ src/myPackage/BOJ14888.java
@@ -0,0 +1,20 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * 연산자 끼워넣기
+ * N개의 수로 이루어진 수열 Ai, 연산자 N-1개(+,-,x,/)
+ * 수의 순서를 바꾸면 안됨
+ * N개의 수와 N-1개의 연산자가 주어졌을 때, 만들수 있는 수식의 최대값과 최솟값 출력
+ * 
+ * 첫째줄: N(2<=N<=11)
+ * 둘째줄: A1, A1, ..., An(1<=Ai<=100)
+ * 셋째줄: 차례대로 +,-,x,/의 갯수(갯수합이 N-1)
+ */
+public class BOJ14888 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
