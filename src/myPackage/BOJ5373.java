17e98d035efb75446b2d09ffbb233c2c2fdc5b94
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-12 14:54:12 +0900

    Update BOJ5373.java

--- src/myPackage/BOJ5373.java
+++ src/myPackage/BOJ5373.java
@@ -1,4 +1,9 @@
 package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
  * 큐빙
  * 큐브가 모두 풀린 상태에서 시작.
@@ -16,7 +21,114 @@ package myPackage;
  * 첫 번째 줄에는 뒷 면과 접하는 칸의 색을 출력하고, 두 번째, 세 번째 줄은 순서대로 출력하면 된다. 흰색은 w, 노란색은 y, 빨간색은 r, 오렌지색은 o, 초록색은 g, 파란색은 b.
  */
 public class BOJ5373 {
+	private static final String NEW_LINE = "\n";
+
+	private static char[][][] cube = new char[5][5][5];
+
 	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		while (tc-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int t = 0; t < n; t++) {
+				char[] way = st.nextToken().toCharArray();
+				rotate(way[0], way[1]);
+			}
+		}
+	}
+	
+	public static void rotate(char base, char d) {
+		if (d == '+') { // 시계방향
+			switch (base) {
+			case 'U':
+				char temp = cube[0][1][1];
+				cube[0][1][1] = cube[0][3][1];
+				cube[0][3][1] = cube[0][3][3];
+				cube[0][3][3] = cube[0][1][3];
+				cube[0][1][3] = temp;
+				
+				temp = cube[0][1][2];
+				cube[0][1][2] = cube[0][2][1];
+				cube[0][2][1] = cube[0][3][2];
+				cube[0][3][2] = cube[0][2][3];
+				cube[0][2][3] = temp;
+				
+				temp = cube[1][4][1];
+				cube[1][4][1] = cube[1][3][4];
+				cube[1][3][4] = cube[1][0][3];
+				cube[1][0][3] = cube[1][1][0];
+				cube[1][1][0] = temp;
+				
+				
+				
+				break;
+				
+			case 'D':
+				break;
+				
+			case 'F':
+				break;
+				
+			case 'B':
+				break;
+				
+			case 'L':
+				break;
+				
+			case 'R':
+				break;
+			}
+		} else { // d == '-' 반시계방향
+			
+		}
+	}
+	
+	public static void lnitialization() {
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= 3; j++) {
+				cube[0][i][j] = 'w';
+				cube[4][i][j] = 'y';
+			}
+		}
 		
+		for (int k = 1; k <= 3; k++) {
+			for (int j = 1; j <= 3; j++) {
+				cube[k][4][j] = 'r';
+				cube[k][0][j] = 'o';
+			}
+		}
+		
+		for (int k = 1; k <= 3; k++) {
+			for (int i = 1; i <= 3; i++) {
+				cube[k][i][0] = 'g';
+				cube[k][i][4] = 'b';
+			}
+		}
+		cube[0][1][1] = 'w'; cube[0][1][2] = 'w'; cube[0][1][3] = 'w';
+		cube[0][2][1] = 'w'; cube[0][2][2] = 'w'; cube[0][2][3] = 'w';
+		cube[0][3][1] = 'w'; cube[0][3][2] = 'w'; cube[0][3][3] = 'w';
+		
+		cube[4][1][1] = 'y'; cube[4][1][2] = 'y'; cube[4][1][3] = 'y';
+		cube[4][2][1] = 'y'; cube[4][2][2] = 'y'; cube[4][2][3] = 'y';
+		cube[4][3][1] = 'y'; cube[4][3][2] = 'y'; cube[4][3][3] = 'y';
+		
+		cube[1][4][1] = 'r'; cube[1][4][2] = 'r'; cube[1][4][3] = 'r';
+		cube[2][4][1] = 'r'; cube[2][4][2] = 'r'; cube[2][4][3] = 'r';
+		cube[3][4][1] = 'r'; cube[3][4][2] = 'r'; cube[3][4][3] = 'r';
+		
+		cube[1][0][1] = 'o'; cube[1][0][2] = 'o'; cube[1][0][3] = 'o';
+		cube[2][0][1] = 'o'; cube[2][0][2] = 'o'; cube[2][0][3] = 'o';
+		cube[3][0][1] = 'o'; cube[3][0][2] = 'o'; cube[3][0][3] = 'o';
+
+		cube[1][1][0] = 'g'; cube[1][2][0] = 'g'; cube[1][3][0] = 'g';
+		cube[2][1][0] = 'g'; cube[2][2][0] = 'g'; cube[2][3][0] = 'g';
+		cube[3][1][0] = 'g'; cube[3][2][0] = 'g'; cube[3][3][0] = 'g';
+
+		cube[1][1][4] = 'b'; cube[1][2][4] = 'b'; cube[1][3][4] = 'b';
+		cube[2][1][4] = 'b'; cube[2][2][4] = 'b'; cube[2][3][4] = 'b';
+		cube[3][1][4] = 'b'; cube[3][2][4] = 'b'; cube[3][3][4] = 'b';
 	}
 }
