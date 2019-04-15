0cb9c9359878882764eb01928fd96e3a134bc13b
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-15 19:29:21 +0900

    Create BOJ17141.java

---
+++ src/myPackage/BOJ17141.java
@@ -0,0 +1,21 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * 연구소2
+ * 특정 위치에 바이러스 M개 놓기
+ * 연구소는 빈칸, 벽으로 이루어짐
+ * 0은 빈 칸, 1은 벽, 2는 바이러스를 놓을 수 있는 곳
+ * 연구소의 상태가 주어졌을 때, 모든 빈 칸에 바이러슨를 퍼뜨리는 최소 시간을 출력.
+ * 
+ * 첫째줄: 연구소의 크기 N(5<=N<=50), 놓을 수 있는 바이러스의 개수 M(2<=M<=10)
+ * 둘째줄~N개줄: 연구소의 상태, 2의 개수는 M보다 크거나 같고, 10보다 작거나 같은 자연수
+ * 바이러스를 모든 칸에 퍼뜨릴 수 없는 경우에는 -1 출력.
+ */
+public class BOJ17141 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
