ae2a2e04baf718978bf7b174c64d07e210d4193e
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-21 19:26:44 +0900

    Update BOJ11004.java

--- src/sort/BOJ11004.java
+++ src/sort/BOJ11004.java
@@ -1,9 +1,11 @@
-package myPackage;
+package sort;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 /*
  * K번째 수
  * 수열 An이 주어질 때, 오름차순 정렬 후 k번째에 있는 수 출력 
@@ -15,13 +17,16 @@ public class BOJ11004 {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
-		int[] A = new int[N + 1];
-		A[0] = Integer.MIN_VALUE;
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
 		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			pq.add(Integer.parseInt(st.nextToken()));
 		}
-		Arrays.sort(A);
-		System.out.println(A[K]);
+		
+		for (int i = 0; i < K - 1; i++) {
+			pq.poll();
+		}
+		System.out.println(pq.poll());
 	}
 }
