8dd16269ac5d7b38de9ade76719bb95234450f53
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-22 12:44:02 +0900

    Create BOJ17100.java

---
+++ src/myPackage/BOJ17100.java
@@ -0,0 +1,15 @@
+package myPackage;
+/*
+ * Candy Boxes
+ * N개의 특가 상품, i번째 특가 상품에는 당도 ai의 사탕 mi개, 가격 ci
+ * 1부터 L까지의 모든 k에 대해서, 당도의 합이 k가 되게 사탕을 먹을 수 있게 구매하는 최소비용출력
+ * 한 특가상품을 구매했을때, 모든 사탕을 먹을 필요는 없다.
+ * 
+ * 첫줄: N과 L이 공백으로 구분되어 주어진다(1<=N,L<=10000)
+ * 둘째줄~N개줄: ai mi ci가 공백으로 구분(1<=ai,mi,ci<=10,000)
+ */
+public class BOJ17100 {
+	public static void main(String[] args) throws Exception {
+		
+	}
+}
