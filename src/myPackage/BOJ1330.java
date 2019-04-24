6eb14891f33d6e2eeda4eddcbd78eed04a87f25f
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-24 19:09:43 +0900

    Create BOJ1330.java

---
+++ src/myPackage/BOJ1330.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * 두 수 비교하기
+ * 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램 작성
+ */
+public class BOJ1330 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
