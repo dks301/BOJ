34a04b04c0510f6aa379c674a1a2fcb134967623
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-04-28 15:55:10 +0900

    Update BOJ5338

--- src/realization/BOJ5338.java
+++ src/realization/BOJ5338.java
@@ -1,7 +1,5 @@
 package realization;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 /*
  * 마크로소프트 로고
  * 마이크로소프트 로고 출력
@@ -12,15 +10,7 @@ import java.io.InputStreamReader;
  *        '-..-'|_.-''-._|
  */
 public class BOJ5338 {
-	private static final String NEW_LINE = "\n";
-	
-	public static void main(String[] args) throws Exception {
-		StringBuilder sb = new StringBuilder();
-		sb.append("       _.-;;-._").append(NEW_LINE);
-		sb.append("'-..-'|   ||   |").append(NEW_LINE);
-		sb.append("'-..-'|_.-;;-._|").append(NEW_LINE);
-		sb.append("'-..-'|   ||   |").append(NEW_LINE);
-		sb.append("'-..-'|_.-''-._|");
-		System.out.print(sb);
+	public static void main(String[] args) {
+		System.out.print("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");
 	}
 }
