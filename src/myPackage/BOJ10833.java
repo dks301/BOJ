eb7da736a44841b523cde90f55d109152fbb879a
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-05-03 15:43:59 +0900

    Create BOJ10833.java

---
+++ src/myPackage/BOJ10833.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * 사과
+ * 각 학교의 학생들에게 나눠주고 남는 사과의 총 개수 출력.
+ */
+public class BOJ10833 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
