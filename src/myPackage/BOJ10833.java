6683fd920f900f7377f92959e0146c86ed05addf
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-05-03 16:08:34 +0900

    Update BOJ10833.java

--- src/myPackage/BOJ10833.java
+++ src/myPackage/BOJ10833.java
@@ -2,6 +2,7 @@ package myPackage;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 /*
  * 사과
  * 각 학교의 학생들에게 나눠주고 남는 사과의 총 개수 출력.
@@ -9,6 +10,9 @@ import java.io.InputStreamReader;
 public class BOJ10833 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+		}
 	}
 }
