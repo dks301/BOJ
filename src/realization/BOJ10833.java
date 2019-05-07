51b4ad6f7199b575b2db468406cc2290bfe2de31
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-05-07 15:52:34 +0900

    Update BOJ10833.java

--- src/realization/BOJ10833.java
+++ src/realization/BOJ10833.java
@@ -1,4 +1,4 @@
-package myPackage;
+package realization;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -11,8 +11,14 @@ public class BOJ10833 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		
+		int total = 0;
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
+			int student = Integer.parseInt(st.nextToken());
+			int apple = Integer.parseInt(st.nextToken());
+			total += (apple % student);
 		}
+		System.out.println(total);
 	}
 }
