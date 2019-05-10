e602a926a712dc94703d5a56c57971adf661a81c
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-05-10 18:01:33 +0900

    Update BOJ17143.java

--- src/myPackage/BOJ17143.java
+++ src/myPackage/BOJ17143.java
@@ -2,6 +2,9 @@ package myPackage;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 /*
  * 낚시왕
  * 낚시왕은 가장 처음에 1번 열의 한 칸 왼쪽에 있다.
@@ -20,9 +23,109 @@ import java.io.InputStreamReader;
  * 둘쨰줄~M개의줄: r,c,s,d,z (상어의 위치(r,c), s 속력, d 이동방향, z크기
  * d: 북-1, 남-2, 동-3, 서-4
  */
+
 public class BOJ17143 {
+	private static int R, C, M;
+	private static int manIdx = -1;
+	private static int total = 0;
+	private static boolean[][] map;
+	private static LinkedList<Shark> ll;
+	private static LinkedList<Shark> ll2;
+	
+	private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+	private static final int ROW = 0;
+	private static final int COL = 1;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
+		map = new boolean[R + 1][C + 1];
+		ll = new LinkedList<>();
+		ll2 = new LinkedList<>();
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			map[r][c] = true;
+			ll.add(new Shark(r, c, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		
+		for (int i = 0; i < C + 1; i++) {
+			fishing();
+			map = new boolean[R + 1][C + 1];
+			Iterator<Shark> it = ll.iterator();
+			while (it.hasNext()) {
+				Shark s = it.next();
+				s.move();
+				if (map[s.r][s.c]) {
+					
+				}
+				map[s.r][s.c] = true;
+				
+			}
+		}
+		
+		System.out.println(total);
+	}
+	
+	public static void fishing() {
+		manIdx++;
+		for (int i = 0; i < R; i++) {
+			if (map[i][manIdx]) {
+				Iterator<Shark> it = ll.iterator();
+				while (it.hasNext()) {
+					Shark s = it.next();
+					if (s.r == i && s.c == manIdx) {
+						ll.remove(s);
+						total += s.z;
+						break;
+					}
+				}
+				break;
+			}
+		}
+	}
+	
+	public static class Shark {
+		int r, c, s, d, z;
+		boolean isDie = false;
+		// 위치(r,c), 속력(s), 이동방향(d), 크기(z)
+		// 1<=r<=R, 1<=c<=C, 0<=s<=1,000, 1<=d<=4, 1<=z<=10,000
+		
+		public Shark(int r, int c, int s, int d, int z) {
+			this.r = r;
+			this.c = c;
+			this.s = s;
+			this.d = d;
+			this.z = z;
+		}
+		
+		public void move() {
+			for (int i = 0; i < s; i++) {
+				int nextRow = r + DIRECTIONS[d - 1][ROW];
+				int nextCol = c + DIRECTIONS[d - 1][COL];
+				if (nextRow < 0) {
+					d = 2;
+					nextRow = 1;
+				} else if (nextRow > R - 1) {
+					d = 1;
+					nextRow = R - 2;
+				} else if (nextCol < 0) {
+					d = 3;
+					nextCol = 1;
+				} else if (nextCol > C - 1) {
+					d = 4;
+					nextCol = C - 2;
+				}
+				
+				r = nextRow;
+				c = nextCol;
+			}
+		}
 	}
 }
