7e5fdea508b19cff38a28a832b3782cae1926f97
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-05-12 17:55:05 +0900

    Update BOJ1107.java

--- src/myPackage/BOJ1107.java
+++ src/myPackage/BOJ1107.java
@@ -14,47 +14,66 @@ import java.util.StringTokenizer;
  * 채널 N으로 이동하기 위해 버튼을 최소 몇 번 눌러야하는지 출력 
  */
 public class BOJ1107 {
-	private static boolean[] numButton = 
-		{true, true, true, true, true, true, true, true, true, true};
-	
+	private static boolean[] brokenButton = {false, false, false, false, false, false, false, false, false, false};
+	private static int[] N = {-1, -1, -1, -1, -1, -1, -1};
+	private static int ans = 0;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int tempN = Integer.parseInt(br.readLine());
+		for (int i = 6; i >= 1; i--) {
+			N[i] = tempN % 10;
+			tempN = tempN / 10;
+			if (tempN == 0) {
+				break;
+			}
+		}
 		int M = Integer.parseInt(br.readLine());
 		
 		if (M != 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			while (st.hasMoreTokens()) {
-				numButton[Integer.parseInt(st.nextToken())] = false;
+				brokenButton[Integer.parseInt(st.nextToken())] = true;
 			}
 		}
 		
-		int ans = Math.abs(N - 100);
-		int div = (int)Math.pow(10, findDigit(N) - 1);
+		for (int i = 0; i <= 9; i++) {
+			if (brokenButton[i]) {
+				int val = findClosest(i);
+			}
+		}
 		
 		
 	}
 	
-	public static void closest(int N, int div) {
-		int val = N / div;
-		if (N / div != 0) {
-			
-		} else {
-			
+	public static int findBottomUp(int n) {
+		int bottom = 0, up = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			bottom++;
+			if (!brokenButton[i]) {
+				break;
+			}
 		}
+		for (int i = n + 1; i <= 9; i++) {
+			up++;
+			if (!brokenButton[i]) {
+				break;
+			}
+		}
+		return bottom > up ? n + up : n - bottom;
 	}
 	
-	public static int findDigit(int N) {
-		if (N == 0) {
-			return 1;
+	public static int findClosest(int n) {
+		int[] closest = {-1, -1, -1, -1, -1, -1, -1};
+		for (int i = 1; i <= 6; i++) {
+			if (N[i] != -1) {
+				if (brokenButton[N[i]]) {
+					closest[i] = findBottomUp(N[i]);
+				} else {
+					ans++;
+					closest[i] = N[i];
+				}
+			}
 		}
-		
-		int cnt = 0;
-		while (N != 0) {
-			N /= 10;
-			cnt++;
-		}
-		return cnt;
 	}
 }
