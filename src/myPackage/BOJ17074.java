768d0b8ee2bde6af8516fe9ed205de7747881b60
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-05-13 17:43:25 +0900

    Create BOJ17074.java

---
+++ src/myPackage/BOJ17074.java
@@ -0,0 +1,17 @@
+package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+ * 정렬
+ * 배열이 주어지고 하나를 버렸을 때, 정렬이 되어있는 경우의 수 출력
+ * 
+ * 첫째줄: 배열의크기N(2<=N<=10^5)
+ * 둘째줄: 배열의원소 ai N개(-10^9<=ai<=10^9)
+ */
+public class BOJ17074 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
