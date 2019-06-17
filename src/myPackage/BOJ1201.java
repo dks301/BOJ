93f2bfbb1a12e0fbf1cbc353e261bcccf8e358ba
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-06-17 18:28:19 +0900

    Update BOJ1201.java

--- src/myPackage/BOJ1201.java
+++ src/myPackage/BOJ1201.java
@@ -26,53 +26,56 @@ public class BOJ1201 {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
+		
 		if (M + K - 1 <= N && N <= M * K) {
 			int[] arr = new int[N + 1];
 			for (int i = 1; i <= N; i++) {
 				arr[i] = i;
 			}
-			ArrayList<Integer>[] group = new ArrayList[M];
-			for (int i = 0; i < M; i++) {
-				group[i] = new ArrayList<>();
+			ArrayList<Integer>[] group = new ArrayList[M + 1];
+			for (int i = 1; i <= M; i++) {
+				group[i] = new ArrayList<Integer>();
 			}
-
+			
 			for (int i = 1; i <= K; i++) {
-				group[0].add(arr[i]);
+				group[1].add(arr[i]);
 			}
-
-			int groupIdx = 1;
-			int lastIdx = K;
-
-			int jump = M != 1 ? (N - K) / (M - 1) : 1;
+			
+			int jump = (M == 1) ? 1 : ((N - K) / (M - 1));
+			int groupIdx = 2;
+			int lastVal = K;
 			for (int i = K + 1; i <= N; i += jump) {
-				if (groupIdx > M - 1) {
-					break;
-				}
 				for (int j = i; j < i + jump; j++) {
 					group[groupIdx].add(arr[j]);
-					lastIdx = j;
+					lastVal = j;
 				}
 				groupIdx++;
-			}
-
-			while (lastIdx != N) {
-				if (group[groupIdx - 1].size() == K) {
-					groupIdx--;
+				if (groupIdx > M) {
+					break;
 				}
-				group[groupIdx - 1].add(arr[++lastIdx]);
 			}
-
-			for (int i = 0; i < M; i++) {
+			
+			groupIdx--;
+			while (lastVal != N) {
+				if (group[groupIdx].size() > K) {
+					groupIdx--;
+					continue;
+				}
+				group[groupIdx].add(++lastVal);
+			}
+			
+			for (int i = 1; i <= M; i++) {
 				Collections.reverse(group[i]);
 			}
-
+			
 			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < M; i++) {
+			for (int i = 1; i <= M; i++) {
 				for (int j = 0; j < group[i].size(); j++) {
-					sb.append(group[i].get(j)).append(SPACE);
+					sb.append(group[i].get(j)).append(SPACE);	
 				}
 			}
 			System.out.println(sb);
+			
 		} else {
 			System.out.println(-1);
 		}
