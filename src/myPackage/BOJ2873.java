d546c481a19691a7c6c4414438d78d143e85498c
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-06-18 21:11:27 +0900

    Update BOJ2873.java

--- src/myPackage/BOJ2873.java
+++ src/myPackage/BOJ2873.java
@@ -1,4 +1,9 @@
 package myPackage;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
  * 롤러코스터
  * 부지는 R행 C열로 이루어짐
@@ -14,5 +19,92 @@ package myPackage;
  * 가장 큰 기쁨을 주는 롤러 코스터는 어떻게 이동하는지 U,R,L,D로 출력
  */
 public class BOJ2873 {
+	private static final String UP = "U";
+	private static final String DOWN = "D";
+	private static final String LEFT = "L";
+	private static final String RIGHT = "R";
 	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		int up = -1, left = -1;
+		for (int i = 1; i <= R; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= C; j++) {
+				int temp = Integer.parseInt(st.nextToken());
+				if (i == R - 1 && j == C) {
+					up = temp;
+				}
+				if (i == R && j == C - 1) {
+					left = temp;
+				}
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		if (R % 2 == 1) {
+			for (int i = 1; i <= R; i++) {
+				for (int j = 1; j <= C; j++) {
+					if (i == R && j == C) {
+						continue;
+					}
+					
+					if (i % 2 == 1) {
+						sb.append(j == C ? DOWN : RIGHT);
+					} else {
+						sb.append(j == C ? DOWN : LEFT);
+					}
+				}
+			}
+		} else if (C % 2 == 1) {
+			for (int j = 1; j <= C; j++) {
+				for (int i = 1; i <= R; i++) {
+					if (i == R && j == C) {
+						continue;
+					}
+					
+					if (j % 2 == 1) {
+						sb.append(i == R ? RIGHT : DOWN);
+					} else {
+						sb.append(i == R ? RIGHT : UP);
+					}
+				}
+			}
+		} else {
+			for (int j = 1; j <= C - 2; j++) {
+				for (int i = 1; i <= R; i++) {					
+					if (j % 2 == 1) {
+						sb.append(i == R ? RIGHT : DOWN);
+					} else {
+						sb.append(i == R ? RIGHT : UP);
+					}
+				}
+			}
+			
+			boolean flag = false;
+			for (int i = 1; i <= R - 2; i++) {
+				for (int j = C - 1; j <= C; j++) {
+					if (i == R - 1 && j == C - 1) {
+						flag = true;
+						break;
+					}
+					
+					if (i % 2 == 1 && j % 2 == 1) {
+						sb.append(j % 2 == 1 ? RIGHT : DOWN);
+					} else {
+						sb.append(j % 2 == 1 ? LEFT : DOWN);
+					}
+					
+				}
+				if (flag) {
+					break;
+				}
+			}
+			sb.append(up > left ? RIGHT + DOWN : DOWN + RIGHT);
+		}
+		System.out.println(sb);
+	}
 }
