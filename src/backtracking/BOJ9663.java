2b4fa711db82a1d23f903616f538d46f4b27fb28
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-07-24 14:39:20 +0900

    Update BOJ9663.java

--- src/backtracking/BOJ9663.java
+++ src/backtracking/BOJ9663.java
@@ -1,4 +1,4 @@
-package myPackage;
+package backtracking;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -15,60 +15,53 @@ import java.io.InputStreamReader;
  */
 public class BOJ9663 {
 	private static int N, ans;
-	private static boolean[][] map;
-	
+	private static boolean[] check_col, check_diag1, check_diag2;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		map = new boolean[N][N];
-		
+		check_col = new boolean[N];
+		check_diag1 = new boolean[N + N];
+		check_diag2 = new boolean[N + N];
+
 		ans = 0;
 		dfs(0);
-		
+
 		System.out.println(ans);
 	}
-	
+
 	public static void dfs(int row) {
 		if (row == N) {
 			ans++;
 			return;
 		}
 		for (int col = 0; col < N; col++) {
-			map[row][col] = true;
 			if (check(row, col)) {
+				check_col[col] = true;
+				check_diag1[row + col] = true;
+				check_diag2[row - col + N - 1] = true;
+				
 				dfs(row + 1);
+				
+				check_col[col] = false;
+				check_diag1[row + col] = false;
+				check_diag2[row - col + N - 1] = false;
 			}
-			map[row][col] = false;
 		}
 	}
-	
+
 	public static boolean check(int row, int col) {
-		for (int i = 0; i < row; i++) { // 위
-			if (map[i][col]) {
-				return false;
-			}
+		if (check_col[col]) {
+			return false;
 		}
 		
-		int x = row - 1;
-		int y = col - 1;
-		while (x >= 0 && y >= 0) { // 왼쪽위 대각선
-			if (map[x][y]) {
-				return false;
-			}
-			x--;
-			y--;
+		if (check_diag1[row + col]) {
+			return false;
 		}
 		
-		x = row - 1;
-		y = col + 1;
-		while (x >= 0 && y < N) { // 오른쪽위 대각선
-			if (map[x][y]) {
-				return false;
-			}
-			x--;
-			y++;
+		if (check_diag2[row - col + N - 1]) {
+			return false;
 		}
-		
 		return true;
 	}
 }
